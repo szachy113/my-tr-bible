7e362e8efc2acf50bf6501fa1180a02fc1712166
Kacper <kacperswirkosz@pm.me>
2022-08-28 19:31:56 +0200

    first build for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-bible-project/',
   plugins: [
     react(),
     svgrPlugin({
